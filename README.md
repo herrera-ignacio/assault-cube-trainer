9f601825c277824408ad32192f82707541e57012
Ignacio Herrera <ignacioromanherrera@gmail.com>
2021-04-25 21:52:40 -0300

    Trainer boilerplate, unlimited pistol ammo

---
+++ README.md
@@ -0,0 +1,13 @@
+# Assault Cube Trainer
+
+You can download Assault Cube [from here](https://assault.cubers.net/).
+
+This trainer is based in the following resources:
+
+* Cyborg Elf's tutorial series:
+    * [#1](https://www.youtube.com/watch?v=oleEyO9nLrQ)
+    * [#2](https://www.youtube.com/watch?v=oleEyO9nLrQ)
+    * [#3](https://www.youtube.com/watch?v=mCC39lRC-LA)
+    * [#4](https://www.youtube.com/watch?v=-hIBZS8Zmdk)
+    * [#5](https://www.youtube.com/watch?v=W2NlCQAIWBM)
+* [Swedish Twat's trainer](https://www.youtube.com/watch?v=w9m0gmcS82Y)
---
+++ actrainer/actrainer/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using Memory;
+
+namespace actrainer
+{
+    public partial class Form1 : Form
+    {
+        Mem meme = new Mem();
+
+        // "ac_client.exe"+00109B74 + 13C
+        string ammoAddress = "ac_client.exe+0x00109B74,13C";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int PID = meme.GetProcIdFromName("ac_client");
+
+            if (PID > 0) // Game opened
+            {
+                meme.OpenProcess(PID);
+                ammoTimer.Start();
+            }
+        }
+
+        private void ammoTimer_Tick(object sender, EventArgs e)
+        {
+            if (ammoCheckbox.Checked)
+            {
+                meme.WriteMemory(ammoAddress, "int", "9999999");
+            }
+        }
+    }
+}
